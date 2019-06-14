6272c8c84cf3f7c20c080609b3f4249c1d1e7196
Chao Zhang <raidenchao@gmail.com>
2019-06-14 14:08:46 -0600

    test

---
+++ website-blocker.py
@@ -0,0 +1,8 @@
+import time
+hosts_path = "C:/Windows/System32/drivers/etc/hosts"
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com","facebook.com","dub119.mail.live.com","www.baidu.com"]
+
+while True:
+    print (1)
+    time.sleep(5)
